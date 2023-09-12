d8dd14f1f38be95c9a058402508848186e38a12b
Rapolas Gruzdys <polasgruzdys@gmail.com>
2023-09-12 17:17:59 +0300

    Implement remaining Devices endpoints

--- tests/ControlDTest.php
+++ tests/ControlDTest.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 use Illuminate\Http\Client\PendingRequest;
 use Rapkis\Controld\ControlD;
+use Rapkis\Controld\Resources\Devices;
 use Rapkis\Controld\Resources\Profiles;
 
 it('accesses profiles resource', function () {
     $client = new ControlD($this->createStub(PendingRequest::class));
     expect($client->profiles())->toBeInstanceOf(Profiles::class);
+});
+
+it('accesses devices resource', function () {
+    $client = new ControlD($this->createStub(PendingRequest::class));
+    expect($client->devices())->toBeInstanceOf(Devices::class);
 });
